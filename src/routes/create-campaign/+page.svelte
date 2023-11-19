c064698a38940e470edb3e13844d3c0642188bae
a14u <aliemre2002@gmail.com>
2023-11-19 11:09:34 +0300

    beacon selection styles

--- src/routes/create-campaign/+page.svelte
+++ src/routes/create-campaign/+page.svelte
@@ -4,6 +4,14 @@
 	import blueBeacon from '$lib/assets/bluebeacon.webp';
 
 	let selectedBeacon = 'none';
+
+	function beaconStyles(selectedBeacon: string, thisBeacon: string) {
+		if (selectedBeacon === thisBeacon) {
+			return 'p-10 border-4 border-green-400 rounded-lg shadow hover:bg-green-200 w-[250px] h-[300px]';
+		} else {
+			return 'p-10 border-4 border-gray-200 rounded-lg shadow hover:bg-gray-100 w-[250px] h-[300px]';
+		}
+	}
 </script>
 
 <form method="POST" action="?/createCampaign">
@@ -15,24 +23,13 @@
 </form>
 
 <div class="flex justify-center items-center mx-20 gap-10">
-	<div
-		class="p-10 border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-		on:click={() => (selectedBeacon = 'yellow')}
-	>
-		<img src={yellowBeacon} width="200" />
+	<div class={beaconStyles(selectedBeacon, 'yellow')} on:click={() => (selectedBeacon = 'yellow')}>
+		<img src={yellowBeacon} />
 	</div>
-	<div
-		class="p-10 border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-		on:click={() => (selectedBeacon = 'green')}
-	>
+	<div class={beaconStyles(selectedBeacon, 'green')} on:click={() => (selectedBeacon = 'green')}>
 		<img src={greenBeacon} width="200" />
 	</div>
-	<div
-		class="p-10 border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-		on:click={() => (selectedBeacon = 'blue')}
-	>
+	<div class={beaconStyles(selectedBeacon, 'blue')} on:click={() => (selectedBeacon = 'blue')}>
 		<img src={blueBeacon} width="200" />
 	</div>
 </div>
-
-{selectedBeacon}
