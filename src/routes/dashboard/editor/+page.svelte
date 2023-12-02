ee1f47c730c5d65cb3ef68287d73ebbccbd06d18
3mre <75805905+mr3mre@users.noreply.github.com>
2023-12-03 01:16:43 +0300

    add editor right content, mobile api directory

--- src/routes/dashboard/editor/+page.svelte
+++ src/routes/dashboard/editor/+page.svelte
@@ -6,6 +6,8 @@
 	let Konva: any;
 	let layer: any;
 	let stage: any;
+	let rightSideContent: boolean = false;
+
 	onMount(async () => {
 		Konva = (await import('konva')).default;
 		const parent: HTMLCanvasElement = document.getElementById('parent')! as HTMLCanvasElement;
@@ -54,6 +56,25 @@
 
 			beacon.on('pointerdblclick', removeBeacon);
 
+			beacon.on('pointerclick', (event: any) => {
+				rightSideContent = true;
+				// console.log(event);
+				// event.target.fill('blue');
+				// layer.draw();
+			});
+
+			beacon.on('pointerenter', (event: any) => {
+				// scale up
+				event.target.scale({ x: 1.2, y: 1.2 });
+				layer.draw();
+			});
+
+			beacon.on('pointerleave', (event: any) => {
+				// scale down
+				event.target.scale({ x: 1, y: 1 });
+				layer.draw();
+			});
+
 			layer.add(beacon);
 			layer.draw();
 
@@ -66,9 +87,64 @@
 	}
 </script>
 
-<div class="relative max-w-max" id="parent">
-	<img src={storePlan} width="600" />
-	<div id="canvas" class="absolute top-0" oncontextmenu="return false" />
+<div class="grid gap-4 grid-cols-2">
+	<div class="relative max-w-max" id="parent">
+		<img src={storePlan} width="600" alt="Store plan" />
+		<div id="canvas" class="absolute top-0" oncontextmenu="return false" />
+	</div>
+	<div
+		class="card w-full bg-base-100 shadow-xl"
+		style={rightSideContent ? 'display: block' : 'display: none'}
+	>
+		<div class="card-body">
+			<h2 class="card-title">
+				<button
+					class="btn btn-ghost btn-circle avatar"
+					on:click={() => {
+						rightSideContent = false;
+					}}
+				>
+					<i class="fa-solid fa-lg fa-xmark" />
+				</button>
+				Beacon
+			</h2>
+			<p>Beacon ID: 123456</p>
+			<p>Beacon UUID: 123456</p>
+			<p>Beacon Major: 123456</p>
+			<p>Beacon Minor: 123456</p>
+			<p>Beacon RSSI: 123456</p>
+			<p>Beacon TX: 123456</p>
+			<p>Beacon Battery: 123456</p>
+			<p>Beacon Temperature: 123456</p>
+			<p>Beacon Last Seen: 123456</p>
+			<p>Beacon Last Updated: 123456</p>
+			<p>Beacon Created: 123456</p>
+			<p>Beacon Updated: 123456</p>
+			<p>Beacon Deleted: 123456</p>
+			<p>Beacon Status: 123456</p>
+			<p>Beacon Name: 123456</p>
+			<p>Beacon Description: 123456</p>
+			<p>Beacon Tags: 123456</p>
+			<p>Beacon Location: 123456</p>
+			<p>Beacon Location Name: 123456</p>
+			<p>Beacon Location Description: 123456</p>
+			<p>Beacon Location Tags: 123456</p>
+		</div>
+		<!-- <figure class="px-10 pt-10">
+			<img
+				src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+				alt="Shoes"
+				class="rounded-xl"
+			/>
+		</figure> -->
+		<div class="card-body">
+			<h2 class="card-title">Campaigns</h2>
+			<p>Beacon ID: 123456</p>
+			<div class="card-actions">
+				<button class="btn btn-primary">Edit</button>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
