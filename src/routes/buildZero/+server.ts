6d4b008a5eb401aab6fb0f05e9668454e702a20c
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-12-08 17:03:57 +0300

    feature: add more default data

--- src/routes/buildZero/+server.ts
+++ src/routes/buildZero/+server.ts
@@ -20,15 +20,35 @@ export const GET: RequestHandler = async () => {
 		userId: 'b3fzlv7vwa7whvz',
 		radius: 0
 	});
+
+	await db.insert(beacons).values({
+		id: 'b270a94d-d16d-4f3d-ac6f-3ce8f66b7cde',
+		userId: 'b3fzlv7vwa7whvz',
+		radius: 0
+	});
+
 	await db.insert(campaigns).values({
 		id: '138757a6-81c5-4279-9fbf-0dcbfe079a92',
 		userId: 'b3fzlv7vwa7whvz',
 		name: 'test campaign'
 	});
+
+	await db.insert(campaigns).values({
+		id: '7abb172d-bbe7-4588-9131-9d1b0f1d3cb5',
+		userId: 'b3fzlv7vwa7whvz',
+		name: 'test campaign'
+	});
+
 	await db.insert(campaignsToBeacons).values({
-		campaignId: '138757a6-81c5-4279-9fbf-0dcbfe079a92',
+		campaignId: '7abb172d-bbe7-4588-9131-9d1b0f1d3cb5',
 		beaconId: '550e8400-e29b-41d4-a716-446655440000'
 	});
+
+	await db.insert(campaignsToBeacons).values({
+		campaignId: '138757a6-81c5-4279-9fbf-0dcbfe079a92',
+		beaconId: 'b270a94d-d16d-4f3d-ac6f-3ce8f66b7cde'
+	});
+
 
 	return new Response('Build successful');
 };
