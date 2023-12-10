04bfedd0de0bcddea2772c05dfdede0b2d217cd1
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-12-10 15:52:37 +0300

    fix: show only active beacons (radius:1,2,3)

--- src/routes/mobile/activeBeacons/+server.ts
+++ src/routes/mobile/activeBeacons/+server.ts
@@ -1,13 +1,13 @@
 import type { RequestHandler } from './$types';
 import { db } from "../../../lib/server/db";
 import {beacons, campaigns, campaignsToBeacons, SelectBeacon} from "../../../schema";
-import {eq} from "drizzle-orm";
+import {eq, not} from "drizzle-orm";
 
 // export async function GET({ url }): Promise<SelectBeacon[]>
 export const GET: RequestHandler = async ({ url }) : Promise<SelectBeacon[]> => {
     try {
         let branchId : string = url.searchParams.get('branchId') ?? '-1';
-        const activeBeacons = await db.select().from(beacons);
+        const activeBeacons = await db.select().from(beacons).where(not(eq(beacons.radius,0)));
         for (const beacon of activeBeacons) {
             const campaignBeacon = await db.select().from(campaignsToBeacons).where(eq(campaignsToBeacons.beaconId, beacon.id)).limit(1);
             const campaign = await db.select().from(campaigns).where(eq(campaigns.id, campaignBeacon[0].campaignId)).limit(1);
