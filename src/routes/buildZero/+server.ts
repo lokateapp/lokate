868222371df2582e36bb40dee73a7f34e97d4038
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-12-10 12:48:29 +0300

    feature: update buildZero to create default objects

--- src/routes/buildZero/+server.ts
+++ src/routes/buildZero/+server.ts
@@ -19,11 +19,12 @@ export const GET: RequestHandler = async ({ url }) => {
 				username: 'asd'
 			}
 		});
-		userId = user.id;
+		userId = user.userId;
 	}
-
+	console.log(userId);
+	const beacon1_id = crypto.randomUUID();
 	await db.insert(beacons).values({
-		id: crypto.randomUUID(),
+		id: beacon1_id,
 		userId: userId,
 		radius: 0,
 		major: '100',
@@ -31,49 +32,37 @@ export const GET: RequestHandler = async ({ url }) => {
 		name: 'test beacon 1'
 	});
 
-	// await db.insert(campaigns).values({
-	// 	id: 'a34c784f-7c44-473e-9810-d521d36d7541',
-	// 	userId: userId,
-	// 	name: 'test campaign'
-	// });
-
-	// await db.insert(campaignsToBeacons).values({
-	// 	campaignId: 'a34c784f-7c44-473e-9810-d521d36d7541',
-	// 	beaconId: '152edb85-4bc6-40a0-a537-9b81967e3eb7'
-	// });
-
+	const beacon2_id = crypto.randomUUID();
 	await db.insert(beacons).values({
-		id: '550e8400-e29b-41d4-a716-446655440000',
-		userId: 'b3fzlv7vwa7whvz',
-		radius: 0
+		id: beacon2_id,
+		userId: userId,
+		radius: 1,
+		major: '100',
+		minor: '12',
+		name: 'test beacon 2'
 	});
 
-	await db.insert(beacons).values({
-		id: 'b270a94d-d16d-4f3d-ac6f-3ce8f66b7cde',
-		userId: 'b3fzlv7vwa7whvz',
-		radius: 0
-	});
-
+	const campaing1_id = crypto.randomUUID();
 	await db.insert(campaigns).values({
-		id: '138757a6-81c5-4279-9fbf-0dcbfe079a92',
-		userId: 'b3fzlv7vwa7whvz',
-		name: 'test campaign'
+		id: campaing1_id,
+		userId: userId,
+		name: 'test campaign1'
 	});
-
+	const campaing2_id = crypto.randomUUID();
 	await db.insert(campaigns).values({
-		id: '7abb172d-bbe7-4588-9131-9d1b0f1d3cb5',
-		userId: 'b3fzlv7vwa7whvz',
-		name: 'test campaign'
+		id: campaing2_id,
+		userId: userId,
+		name: 'test campaign2'
 	});
 
 	await db.insert(campaignsToBeacons).values({
-		campaignId: '7abb172d-bbe7-4588-9131-9d1b0f1d3cb5',
-		beaconId: '550e8400-e29b-41d4-a716-446655440000'
+		campaignId: campaing1_id,
+		beaconId: beacon1_id
 	});
 
 	await db.insert(campaignsToBeacons).values({
-		campaignId: '138757a6-81c5-4279-9fbf-0dcbfe079a92',
-		beaconId: 'b270a94d-d16d-4f3d-ac6f-3ce8f66b7cde'
+		campaignId: campaing2_id,
+		beaconId: beacon2_id
 	});
 
 	return new Response('Build successful');
