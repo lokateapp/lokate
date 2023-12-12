1c88730f3b7c9bcd0965a8d9d16931e41abe403b
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-12-12 18:28:04 +0300

    feature: remove all data at once ( order of db delete is important)

---
+++ src/routes/database/removeAllData/+server.ts
@@ -0,0 +1,22 @@
+import { beacons, campaigns, campaignsToBeacons, user, customers, session, key, events } from '../../../schema';
+import { db } from "../../../lib/server/db";
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async ({ }) => {
+	try {
+		await db.delete(events);
+		await db.delete(customers);
+		await db.delete(campaigns);
+		await db.delete(campaignsToBeacons);
+		await db.delete(beacons);
+		await db.delete(key);
+		await db.delete(session);
+		await db.delete(user);
+
+		// Use Response constructor with a null body and status code 200
+		return new Response("✅ Database emptied", { status: 200 });
+	} catch (error) {
+		// Use Response constructor with a null body and status code 500
+		return new Response("❌ Error emptying the database: " + error, { status: 500 });
+	}
+};
