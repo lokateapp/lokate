568b01a1f99f66faca73b96a48a9a2cd97cf14ff
3mre <75805905+mr3mre@users.noreply.github.com>
2023-12-12 16:32:39 +0300

    analytics event backend

---
+++ src/routes/dashboard/analytics/+page.server.ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { db } from '../../../lib/server/db';
+import {
+	events,
+	campaigns,
+    customers
+} from '../../../schema';
+import { eq } from 'drizzle-orm';
+import type { PageServerLoad } from '../$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.auth.validate();
+	const events = getEvents(session.user.userId);
+
+
+    console.log("events: ", events);
+
+	return { events };
+};
+
+async function getEvents(userId: string): Promise<any[]> {
+	return await db
+		.select()
+		.from(events)
+		.innerJoin(campaigns, eq( events.campaignId, campaigns.id))
+		.innerJoin(customers, eq(events.customerId, customers.id))
+		.where(eq(campaigns.userId, userId));
+}
